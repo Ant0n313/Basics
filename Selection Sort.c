fbc33eb30781b1827f45c836d97d1d56281b787c
Anton Ashraf <51335125+Ant0n313@users.noreply.github.com>
2021-11-24 02:36:20 +0200

    Create Selection Sort.c

---
+++ Selection Sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+// Selection Sort 
+// Worst Complexity O(n^2)
+
+void SelectionSort (int A[], int n) {
+  int temp;
+  for (int i = 0; i < n; i++){
+    int minidx = i; 
+    for (int j = i+1; j < n; j++){ // start from i + 1 to see what is the next minimum element
+      if (A[j] < A[minidx]) 
+        minidx = j;
+    }
+    temp = A[minidx]; // swap it at the end after finding the mininmum
+    A[minidx] = A[i];
+    A[i] = temp;
+  }
+}
+
+void printArray(int A[],int n) {
+  for (int i = 0; i < n; i++)
+    printf("%d ",A[i]);
+}
+
+int main(void){
+
+  int A[] = {4,2,18,1,6,7};
+  int n = sizeof(A) / sizeof(A[0]); // to get the size of the array
+
+  SelectionSort(A,n);
+  printArray(A,n);
+
+  return 0;
+}
+
