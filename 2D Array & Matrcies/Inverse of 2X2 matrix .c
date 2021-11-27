5c199d2224dfdc98a0ebbe2485e6e60bae4ff702
Anton Ashraf <51335125+AntonAshraf@users.noreply.github.com>
2021-11-27 18:46:59 +0200

    Create Inverse of 2X2 matrix .c

---
+++ 2D Array & Matrcies/Inverse of 2X2 matrix .c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define N 100
+
+void inverse_2X2 (int m1[][N], int r, int c){
+  if(r == 2 && c == 2){
+    float det = (m1[0][0] * m1[1][1]) - (m1[0][1] * m1[1][0]);
+
+    if (det){ //if det != 0 (aka the matrix has an inverse)
+    
+      float ans[2][2];
+
+      ans[0][0] = m1[1][1];
+      ans[0][1] = -m1[0][1];
+      ans[1][0] = -m1[1][0];
+      ans[1][1] = m1[0][0];
+
+      printf("The inverse of the matrix is:\n");
+      for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++)
+          printf("%.2f ", (ans[i][j] / det));
+        printf("\n");
+      }
+
+    }
+    else
+      printf("Matrix A doesn't have an Inverse\n");
+  }
+}
+
+int main () {
+  int mat1[N][N], r, c;
+ 
+  printf("please enter rows and coulmns of first matrix: ");
+  scanf("%d%d",&r,&c);
+  
+  printf ("enter the first matrix %d X %d\n", r, c);
+  for (int i = 0; i < r; i++)
+    for (int j = 0; j < c; j++)
+      scanf("%d",&mat1[i][j]);
+ 
+  inverse_2X2(mat1,r,c);
+
+
+  return 0;
+}
+
+/* 
+  sample input:
+
+  2 2
+
+  3 1 
+  4 2
+
+ */
