4ff91c9a6c2516202b89b40fc323b948693de9f9
anton <anton0ashraf@gmail.com>
2022-04-20 03:26:55 +0200

    init

---
+++ Sorting/Insersion Sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// Selection Sort 
+// Worst Complexity O(n^2)
+
+void InsertonSort (int A[], int n) {
+  int key, j;
+  for (int i = 0; i < n; i++){
+    key = A[i];
+    j = i - 1;
+    while (j >= 0 && A[j] > key) {
+      A[j + 1] = A[j];
+      j--;
+    }
+    A[j + 1] = key;
+  }
+}
+
+void printArray(int A[],int n) {
+  for (int i = 0; i < n; i++)
+    printf("%d ",A[i]);
+}
+
+int main(void){
+
+  int A[] = {4,2,18,1,6,7,-3,0};
+  int n = sizeof(A) / sizeof(A[0]); // to get the size of the array
+
+  InsertonSort(A,n);
+  printArray(A,n);
+
+  return 0;
+}
+
